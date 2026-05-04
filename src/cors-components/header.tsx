ed823f1209429a7fcedfa8fb11acc9704122d4e4
João <joaopedrocararinerangel@gmail.com>
2026-05-04 18:12:00 -0300

    style: ajusta interface de navegação e busca

--- src/cors-components/header.tsx
+++ src/cors-components/header.tsx
@@ -1,27 +1,33 @@
 import Container from "../components/container";
 import Logo from "../assets/images/galeria-plus-full-logo.svg?react"
 import Button from "../components/button";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import cx from "classnames";
 import PhotosSearch from "./photos-search";
 import Divider from "../components/divider";
 import PhotoNewDialog from "../contexts/photos/components/photo-new-dialog";
+import AlbumsNewDialog from "../contexts/albums/components/albums-new-dialog";
 
 interface HeaderProps extends React.ComponentPropsWithoutRef<typeof Container> {
 
 }
 
 export default function Header({className, ...props}: HeaderProps) {
+    const {pathname} = useLocation();
     return (
         <Container as="header" className={cx("flex justify-between items-center gap-10", className)} {...props}>
             <Link to="/">
             <Logo className="h-5" />
             </Link>
-            <PhotosSearch />
-            <Divider orientation="vertical" className="h-10" />
+            {pathname === '/' && (
+                <>
+                <PhotosSearch />
+                <Divider orientation="vertical" className="h-10" />
+                </>
+            )}
             <div className="flex items-center gap-3">
             <PhotoNewDialog trigger={<Button>Nova foto</Button>}/>
-            <Button variant="secondary">Criar álbum</Button>
+            <AlbumsNewDialog trigger={<Button variant="secondary">Criar álbum</Button>}/>
             </div>
         </Container>
     )
--- src/cors-components/photos-search.tsx
+++ src/cors-components/photos-search.tsx
@@ -2,14 +2,16 @@ import InputText from "../components/input-text";
 import Search from "../assets/icons/search.svg?react";
 import { debounce } from "../helpers/utils";
 import React from "react";
+import usePhotos from "../contexts/photos/hooks/use-photos";
 
 export default function PhotosSearch({}) {
     const [inputValue, setInputValue] = React.useState("");
+    const {filters} = usePhotos();
     const deboncedSetValue = React.useCallback(
         debounce((value: string) => {
-            console.log("Search for", value);
+            filters.setQ(value);
         }, 500),
-        []
+        [filters.setQ]
     );
 
     function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
